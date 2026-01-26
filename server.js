b7eff676f57d6a522bae18639d953fd540fc9356
CesarSantosCasado <dg@coajmadrid.es>
2026-01-26 11:28:02 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // URLs
-const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbxSgNVZou0wYkiY8XlehafsU_ge6kpwNjRSVQyEPvdCwiC1tdxR-MBEj4-EVnsz5Zg4sQ/exec";
+const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbxk7JZywLmy7l6luUOqrZSPfaeAmOEHhJydfODqaVGjL7f28hhsCGhGEMEWqYEMAAOxBw/exec";
 
 const CONFIG = {
   appId: "b6fac65a-32b5-445f-8831-d6f1be2b4433",
